2892f621f3b0cc553b4e4e6cea5f2bb8b2d1e75f
Viduni Randima <mahaduravidur@gmail.com>
2020-04-12 10:49:27 +0530

    Initial commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function all(){
+        $response = Product::all();
+        
+        return response($response->toJson())->header('Content-Type', 'application/json');
+    
+    }
+}
